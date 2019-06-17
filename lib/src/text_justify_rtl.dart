533ee01cff2949b1fbcee0e699ed5c8baaf2930c
Sahand <sahandevs@gmail.com>
2019-06-17 09:14:22 +0430

    init

---
+++ lib/src/text_justify_rtl.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class TextJustifyRTL extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
