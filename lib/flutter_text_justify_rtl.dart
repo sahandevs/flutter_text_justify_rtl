70bea73c65d0613f952a755e5276161f5c01c7f4
Sahand <sahandevs@gmail.com>
2019-06-17 09:21:30 +0430

    connect example project to library

--- lib/flutter_text_justify_rtl.dart
+++ lib/flutter_text_justify_rtl.dart
@@ -1,7 +1,3 @@
 library flutter_text_justify_rtl;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/text_justify_rtl.dart';
--- lib/src/text_justify_rtl.dart
+++ lib/src/text_justify_rtl.dart
@@ -1,10 +1,13 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class TextJustifyRTL extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-
+      color: Colors.blue,
+      width: 20,
+      height: 20,
     );
   }
 }
